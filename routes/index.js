83094ab70334db0822b0ed5d29665589627c9cbe
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-05-02 15:05:04 -0400

    reduced default text size

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const mainRoutes = require("./mainRoutes");
 const artItemRoutes = require("./artItemRoutes");
 const constructorMethod = (app) => {
-  app.use("/", mainRoutes);
-  app.use("/item", artItemRoutes);
-  app.use("*", (req, res) => {
-    res.sendStatus(404);
-  });
+	app.use("/", mainRoutes);
+	app.use("/item", artItemRoutes);
+	app.use("*", (req, res) => {
+		res.sendStatus(404);
+	});
 };
 
 module.exports = constructorMethod;
