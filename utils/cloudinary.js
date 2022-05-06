02f8f68853b10c177b62730019a7a2cd7eea5e51
Eric Rudzin <55248337+erudzin@users.noreply.github.com>
2022-05-06 18:07:10 -0400

    took out info

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,10 +1,6 @@
 //https://github.com/itsmefarhan/nodejs-cloudinary-crud-operations/blob/master/routes/user.js
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: "art-point",
-  api_key: "441664597625179",
-  api_secret: "4KF7kF-or8xea0oCXPq2XYqXWwM",
-});
+cloudinary.config({});
 
 module.exports = cloudinary;
