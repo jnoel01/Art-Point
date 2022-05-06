6aea4a47c856c0aff31135b341d86d3eda0f3e2d
Shindi <vendingmachinesfullofsnacks@outlook.com>
2022-05-06 12:59:01 -0400

    pushing to cindy
    i think.

--- data/users.js
+++ data/users.js
@@ -1,19 +1,16 @@
 const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
 const { ObjectId } = require("mongodb");
 
 let exportedMethods = {
 	async createUser(
 		profilePic,
-		userName,
-		firstName,
-		lastName,
-		password,
-		email,
-		dob
+		userName, password,
+		firstName, lastName,
+		email, dob
 	) {
+		console.log("attemping to insert a user.");
 		const userCollection = await users();
 
 		// ---------------- checks if instances are here. -------------
@@ -39,147 +36,117 @@ let exportedMethods = {
 		if (email.trim().length === 0)
 			throw "Email URL cannot just be empty spaces!";
 
-		// ------------- checks if strings have unwanted spaces --------
+		// ------------- checks if strings have unwanted characters --------
+		var regEx = /^[0-9a-zA-Z]+$/;
+		if (!userName.match(regEx)) throw "Username must only contain alphanumeric characters!";
+		if (!firstName.match(regEx)) throw "First Name must only contain alphanumeric characters!";
+		if (!lastName.match(regEx)) throw "Last Name must only contain alphanumeric characters!";
+		
+		// ------------- checks if length of strings are enough ------------
+		if (userName.length < 3) throw "Username must be at least 3 characters long.";
+		if (firstName.length < 3) throw "First Name must be at least 3 characters long.";
+		if (lastName.length < 3) throw "Last Name must be at least 3 characters long.";
+		if (password.length < 8) throw "Password must be at least 8 characters long.";
+		if (email.length < 8) throw "Email must be at least 8 characters long.";
 
-		for (x of username) {
-			if (x === " ") {
-				throw "Username cannot have spaces!";
-			}
-		}
-		var regEx = /^[0-9a-zA-Z]+$/; //checking for alphanumeric only. got regex from w3schools!
-		if (!username.match(regEx)) {
-			throw "Username must only contain alphanumeric characters!";
-		}
-		if (username.length < 4) {
-			throw "Username must be at least 6 characters long.";
-		}
-		let usernameCheck = username.toLowerCase();
+		// -------------- checks if username already exist -----------------
+		let usernameCheck = userName.toLowerCase();
 		const dupeCheck = await userCollection.findOne({ username: usernameCheck });
-		if (dupeCheck) {
-			throw "This username already exists! Try another one.";
-		}
-		if (password.trim().length === 0) {
-			throw "Password cannot just be empty spaces!";
-		}
-		for (x of password) {
-			if (x === " ") {
-				throw "Password cannot have spaces!";
-			}
-		}
-		if (password.length < 6) {
-			throw "Password must be at least 6 characters long.";
-		}
+		if (dupeCheck) throw "This username already exists! Try another one.";
 
+		// -------------- get the date they created the account ------------------
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, '0');
+		var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+		var yyyy = today.getFullYear();
+
+		today = mm + '/' + dd + '/' + yyyy;
+
+		// --------------- inserting user's information into database --------
+		let hashedPassword = await bcrypt.hash(password, 16)
 		let userInserted = {
-			username: username.toLowerCase(),
-			password: password,
+			profilePic: profilePic,
+			userName: userName.toLowerCase(),
+			firstName: firstName,
+			lastName: lastName,
+			hashedPassword: hashedPassword,
+			userEmail: email,
+			userDob: dob,
+			dateJoined: "today",
+			ownedArt: [],
+			userPurchases: []
 		};
 
-		let hashPass = await bcrypt.hash(userInserted.password, saltRounds);
-
-		userInserted.password = hashPass;
-
 		const newInsertInformation = await userCollection.insertOne(userInserted);
-		if (!newInsertInformation.insertedId) {
-			throw "Insert failed!";
-		}
+		if (!newInsertInformation.insertedId) throw "Insert failed!";
+
 		return await { userInserted: true };
 	},
 	async checkUser(username, password) {
 		const userCollection = await users();
 
-		if (!username) {
-			throw "Username must be supplied!";
-		}
-		if (!password) {
-			throw "Password must be supplied!";
-		}
-		if (username.trim().length === 0) {
-			throw "Username cannot just be empty spaces!";
-		}
+		// ---------------- error checking -------------
+		if (!username) throw "Username must be supplied!";
+		if (!password) throw "Password must be supplied!";
+	
+		if (username.trim().length === 0) throw "Username cannot just be empty spaces!";
+		if (password.trim().length === 0) throw "Password cannot just be empty spaces!";
+
+		if (username.length < 3) throw "Username must be at least 3 characters long.";
+		if (password.length < 8) throw "Password must be at least 8 characters long.";
+		
 		for (x of username) {
-			if (x === " ") {
-				throw "Username cannot have spaces!";
-			}
-		}
-		var regEx = /^[0-9a-zA-Z]+$/; //checking for alphanumeric only. got regex from w3schools!
-		if (!username.match(regEx)) {
-			throw "Username must only contain alphanumeric characters!";
-		}
-		if (username.length < 4) {
-			throw "Username must be at least 6 characters long.";
-		}
-		if (password.trim().length === 0) {
-			throw "Password cannot just be empty spaces!";
+			if (x === " ") throw "Username cannot have spaces!";
 		}
 		for (x of password) {
-			if (x === " ") {
-				throw "Password cannot have spaces!";
-			}
-		}
-		if (password.length < 6) {
-			throw "Password must be at least 6 characters long.";
+			if (x === " ") throw "Password cannot have spaces!";
 		}
 
+		var regEx = /^[0-9a-zA-Z]+$/; 
+		if (!username.match(regEx)) throw "Username must only contain alphanumeric characters!";
+		
+		// --------- finding user ----------
 		let usernameLower = username.toLowerCase();
 
 		let userToCheck = await userCollection.findOne({ username: usernameLower });
-		if (!userToCheck) {
-			throw "Either the username or password is invalid";
-		}
-
+		if (!userToCheck) throw "Either the username or password is invalid";
+		
+		// ---------- authenticating -----------
 		let checkPassword = await bcrypt.compare(password, userToCheck.password);
-
-		if (checkPassword) {
-			return { authenticated: true };
-		} else {
-			throw "Either the username or password is invalid";
-		}
+		if (checkPassword) return { authenticated: true };
+ 
+		throw "Either the username or password is invalid";
 	},
 	async resetPassword(username, password) {
 		const userCollection = await users();
-		if (!username) {
-			throw "Username must be supplied!";
-		}
-		if (!password) {
-			throw "New password must be supplied!";
-		}
-		if (username.trim().length === 0) {
-			throw "Username cannot just be empty spaces!";
-		}
+
+		// ---------------- error checking -------------
+		if (!username) throw "Username must be supplied!";
+		if (!password) throw "Password must be supplied!";
+	
+		if (username.trim().length === 0) throw "Username cannot just be empty spaces!";
+		if (password.trim().length === 0) throw "Password cannot just be empty spaces!";
+
+		if (username.length < 3) throw "Username must be at least 3 characters long.";
+		if (password.length < 8) throw "Password must be at least 8 characters long.";
+		
 		for (x of username) {
-			if (x === " ") {
-				throw "Username cannot have spaces!";
-			}
-		}
-		var regEx = /^[0-9a-zA-Z]+$/; //checking for alphanumeric only. got regex from w3schools!
-		if (!username.match(regEx)) {
-			throw "Username must only contain alphanumeric characters!";
-		}
-		if (username.length < 4) {
-			throw "Username must be at least 6 characters long.";
-		}
-		if (password.trim().length === 0) {
-			throw "New password cannot just be empty spaces!";
+			if (x === " ") throw "Username cannot have spaces!";
 		}
 		for (x of password) {
-			if (x === " ") {
-				throw "New password cannot have spaces!";
-			}
-		}
-		if (password.length < 6) {
-			throw "New password must be at least 6 characters long.";
+			if (x === " ") throw "Password cannot have spaces!";
 		}
 
+		var regEx = /^[0-9a-zA-Z]+$/; 
+		if (!username.match(regEx)) throw "Username must only contain alphanumeric characters!";
+		
+		// --------- finding user ----------
 		let usernameLower = username.toLowerCase();
 
 		let userToCheck = await userCollection.findOne({ username: usernameLower });
+		if (!userToCheck) throw "Could not find this username. Please try another!";
 
-		if (!userToCheck) {
-			throw "Could not find this username. Please try another!";
-		}
-
-		let hashPass = await bcrypt.hash(password, saltRounds);
+		let hashPass = await bcrypt.hash(password, 16);
 
 		userCollection.updateOne(
 			{ username: usernameLower },
@@ -191,6 +158,17 @@ let exportedMethods = {
 		);
 		return await { passwordUpdated: true };
 	},
+	async getUserBySearch(userSearched) {
+		if(!userSearched) throw "Search term is not provided.";
+
+		const userCollection = await users();
+
+		let usersFound = [];
+		for (x of data) {
+			usersFound.push(x);
+		}
+		return usersFound;
+	}
 };
 
 module.exports = exportedMethods;
