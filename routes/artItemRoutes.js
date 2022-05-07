9d7a1745b635af1f590924237c82b571ea1da46d
Eric Rudzin <55248337+erudzin@users.noreply.github.com>
2022-05-07 17:14:54 -0400

    fixing artist username in art item

--- routes/artItemRoutes.js
+++ routes/artItemRoutes.js
@@ -23,58 +23,58 @@ const upload = require("../utils/multer");
 //   });
 // });
 router.get("/:id", async (req, res) => {
-	let artId = req.params.id;
-	//console.log(artId);
-	try {
-		let art = await artItemApi.getArtItemById(artId);
-		let artist = await userData.getUser(art.userId);
-		console.log(artist);
-		res.render("../views/pages/artItem", {
-			artTitle: art.artTitle,
-			imageSource: art.imageSource,
-			artist: artist.userName,
-			artRating: art.artRating,
-			typeGenre: art.typeGenre,
-			forSale: art.forSale,
-			setPrice: art.setPrice,
-			purchasePage: "../views/pages/purchaseItem",
-		});
-	} catch (e) {
-		res.render("../views/pages/error", { error: e });
-	}
+  let artId = req.params.id;
+  //console.log(artId);
+  try {
+    let art = await artItemApi.getArtItemById(artId);
+    let artist = await userData.getUser(ObjectId(art.userId));
+    //console.log(artist);
+    res.render("../views/pages/artItem", {
+      artTitle: art.artTitle,
+      imageSource: art.imageSource,
+      artist: artist.userName,
+      artRating: art.artRating,
+      typeGenre: art.typeGenre,
+      forSale: art.forSale,
+      setPrice: art.setPrice,
+      purchasePage: "../views/pages/purchaseItem",
+    });
+  } catch (e) {
+    res.render("../views/pages/error", { error: e });
+  }
 });
 
 router.post("/submitart", upload.single("image"), async (req, res) => {
-	//console.log("inside submit art");
-	try {
-		let artSubmissionInfo = req.body;
-		console.log("user" + req.session.userId);
-		const result = await cloudinary.uploader.upload(req.file.path);
+  //console.log("inside submit art");
+  try {
+    let artSubmissionInfo = req.body;
+    console.log("user" + req.session.userId);
+    const result = await cloudinary.uploader.upload(req.file.path);
 
-		const newArtSubmission = await artItemApi.createArtItem(
-			req.session.userId,
-			artSubmissionInfo.artTitle,
-			artSubmissionInfo.forSale,
-			artSubmissionInfo.setPrice,
-			0,
-			artSubmissionInfo.typeGenre,
-			result.secure_url,
-			result.public_id
-		);
-		//console.log(newArtSubmission);
-		res.status(200).redirect("/item/" + newArtSubmission._id);
-	} catch (e) {
-		//console.log("hi");
-		//console.log(e);
-		res.status(400).json({ error: e });
-	}
+    const newArtSubmission = await artItemApi.createArtItem(
+      req.session.userId,
+      artSubmissionInfo.artTitle,
+      artSubmissionInfo.forSale,
+      artSubmissionInfo.setPrice,
+      0,
+      artSubmissionInfo.typeGenre,
+      result.secure_url,
+      result.public_id
+    );
+    //console.log(newArtSubmission);
+    res.status(200).redirect("/item/" + newArtSubmission._id);
+  } catch (e) {
+    //console.log("hi");
+    //console.log(e);
+    res.status(400).json({ error: e });
+  }
 });
 router.post("/rateArt", async (req, res) => {
-	try {
-		let newRating = req.body.rating;
-		console.log(newRating);
-	} catch (e) {
-		res.json(e);
-	}
+  try {
+    let newRating = req.body.rating;
+    console.log(newRating);
+  } catch (e) {
+    res.json(e);
+  }
 });
 module.exports = router;
